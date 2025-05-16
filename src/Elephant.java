cc83a764e4ef7f29814c7270a7025e1e8e16b7c0
s34515 <s34515@pjwstk.edu.pl>
2025-05-16 10:51:26 +0200

    school work

---
+++ src/Elephant.java
@@ -0,0 +1,42 @@
+public class Elephant extends Animal {
+    int weight;
+    public Elephant(String name, int age, int weight) {
+        super(name, age);
+        this.weight = weight;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    @Override
+    public void makeSound() {
+        System.out.println("Elephant Trumpet sound");
+        System.out.println();
+    }
+    @Override
+    public void move() {
+        System.out.println("Elephant moves");
+        System.out.println("THUMP THUMP THUMP THUMP");
+        System.out.println();
+    }
+    @Override
+    public void displayInformation() {
+        System.out.println("Elephant Name: " + getName());
+        System.out.println("Elephant Age: " + getAge());
+        System.out.println("Elephant Weight: " + weight);
+        System.out.println("Elephants are the largest living land animals." +
+                " Three living species are currently recognised:" +
+                " the African bush elephant (Loxodonta africana)," +
+                " the African forest elephant (L. cyclotis)," +
+                " and the Asian elephant (Elephas maximus)." +
+                " They are the only surviving members of the family Elephantidae and the order Proboscidea;" +
+                " extinct relatives include mammoths and mastodons." +
+                " Distinctive features of elephants include a long proboscis called a trunk, tusks, large ear flaps, pillar-like legs, and tough but sensitive grey skin.");
+        System.out.println();
+    }
+    @Override
+    public void naturalHabitat() {
+        System.out.println("Elephant natural habitat");
+        System.out.println("Elephants primarly live in african and in asia, in the bush and in the jungle");
+        System.out.println();
+    }
+}
---
+++ src/Snake.java
@@ -0,0 +1,47 @@
+public class Snake extends Animal {
+    int length;
+    Boolean venomous;
+    public Snake(String name, int age, int length, Boolean venimous) {
+        super(name, age);
+        this.length = length;
+        this.venomous = venimous;
+    }
+    public int getLength() {
+        return length;
+    }
+    public String isVenimous() {
+        return venomous.booleanValue() ? "yes" : "no";
+    }
+    @Override
+    public void makeSound() {
+        System.out.println("Hisssssssssssssss");
+        System.out.println();
+    }
+    @Override
+    public void move() {
+        System.out.println("Snake moves");
+        System.out.println("slither slither");
+        System.out.println();
+    }
+    @Override
+    public void displayInformation() {
+        System.out.println("Snake Name: " + getName());
+        System.out.println("Snake Age: " + getAge());
+        System.out.println("Snake Weight: " + getLength());
+        System.out.println("Snake Venimous?: " + isVenimous());
+        System.out.println("Snakes are elongated limbless reptiles of the suborder Serpentes." +
+                " Cladistically squamates, snakes are ectothermic, amniote vertebrates covered in overlapping scales much like other members of the group." +
+                " Many species of snakes have skulls with several more joints than their lizard ancestors and relatives," +
+                " enabling them to swallow prey much larger than their heads (cranial kinesis)." +
+                " To accommodate their narrow bodies, snakes' paired organs (such as kidneys) appear one in front of the other instead of side by side," +
+                " and most only have one functional lung.");
+        System.out.println();
+    }
+    @Override
+    public void naturalHabitat() {
+        System.out.println("Snake's natural habitat");
+        System.out.println("Snakes are found on every continent with the exception of Antartica." +
+                "The only excemption to this are some larger islands such as greenland, New Zealand, and a few more");
+        System.out.println();
+    }
+}
